e53a4ba470f068dd75d485ca793b2f612b39c8a6
Ficus28 <sutniktv@gmail.com>
2025-03-15 17:48:57 +0300

    Заверешние первой части проекта

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
     name: "Архыз",
-    link: "https://my-server/cards-nameproject/arkhyz.jpg",
+    link: "https://avatars.mds.yandex.net/get-ydo/3912055/2a0000018fafbdb99acbe0a7f5bf667a1e69/diploma",
   },
   {
     name: "Челябинская область",
-    link: "https://my-server/cards-nameproject/chelyabinsk-oblast.jpg",
+    link: "https://avatars.mds.yandex.net/i?id=8bc78a51eb3ee88607e761fd8b414a99_l-5252229-images-thumbs&n=13",
   },
   {
     name: "Иваново",
-    link: "https://my-server/cards-nameproject/ivanovo.jpg",
+    link: "https://happylove.top/uploads/posts/2023-09/1694171941_happylove-top-p-krasivie-foto-goroda-ivanovo-instagram-49.jpg",
   },
   {
     name: "Камчатка",
-    link: "https://my-server/cards-nameproject/kamchatka.jpg",
+    link: "https://gkecopoldnr.ru/wp-content/uploads/2024/09/medved-e1725620738523.jpg",
   },
   {
     name: "Холмогорский район",
-    link: "https://my-server/cards-nameproject/kholmogorsky-rayon.jpg",
+    link: "https://avatars.mds.yandex.net/i?id=496c793e2fd915139278ab174f3ae38b_l-5232340-images-thumbs&n=13",
   },
   {
     name: "Байкал",
-    link: "https://my-server/cards-nameproject/baikal.jpg",
+    link: "https://xn--80aagchebveo1advbvqjs.xn--p1ai/images/content_hotel_56fd1e39742fe8.00271358.jpg",
   }
 ];
 
